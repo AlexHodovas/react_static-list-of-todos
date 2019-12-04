af04d96a07f34be3a7a58d443857e1bacc467845
Alex Hodovas <o.hodovas@gmail.com>
2019-12-04 15:29:03 +0200

    Updates

--- precache-manifest.ac67278606c7027dd5dbc99c7ca1b207.js
+++ precache-manifest.ac67278606c7027dd5dbc99c7ca1b207.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9f4c0541055d1cb80dc84d525e26cd92",
+    "revision": "67301bafd6c1f93a30ae21f8e48a9c6c",
     "url": "/react_static-list-of-todos/index.html"
   },
   {
-    "revision": "b40e9ca27fda17825a70",
+    "revision": "7c67f9ddeea0ad3cc1b8",
     "url": "/react_static-list-of-todos/static/css/main.cb9e985a.chunk.css"
   },
   {
-    "revision": "2315f74ce3c99316133d",
-    "url": "/react_static-list-of-todos/static/js/2.3310f33a.chunk.js"
+    "revision": "d1bf606ea68b30240b50",
+    "url": "/react_static-list-of-todos/static/js/2.a7975bab.chunk.js"
   },
   {
-    "revision": "b40e9ca27fda17825a70",
-    "url": "/react_static-list-of-todos/static/js/main.1d2983b2.chunk.js"
+    "revision": "7c67f9ddeea0ad3cc1b8",
+    "url": "/react_static-list-of-todos/static/js/main.d7bb5fec.chunk.js"
   },
   {
     "revision": "ea6f31786c2aed313835",
